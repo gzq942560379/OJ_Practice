5d2ca65e215c33cac6886ab9809b8eb6fe803b88
gzq <gzq9425@qq.com>
2020-09-21 09:39:30 +0800

    the kth number

--- src/the_kth_number.cpp
+++ src/the_kth_number.cpp
@@ -4,13 +4,33 @@
 
 using namespace std;
 
-int main(){
-    int m,n;
-    cin >> m >> n;
-    vector<int> vec(m);
-    for(int i = 0;i<m;i++){
+int the_kth_number_sort(vector<int> &vec, int k)
+{
+    sort(vec.begin(), vec.end());
+    return vec[vec.size() - k];
+}
+
+int the_kth_number_heap(vector<int> &vec, int k)
+{
+    make_heap(vec.begin(), vec.end());
+    while (k > 1)
+    {
+        pop_heap(vec.begin(), vec.end());
+        vec.pop_back();
+        k -= 1;
+    }
+    pop_heap(vec.begin(), vec.end());
+    return vec.back();
+}
+
+int main()
+{
+    int size, k;
+    cin >> size >> k;
+    vector<int> vec(size);
+    for (int i = 0; i < size; i++)
+    {
         cin >> vec[i];
     }
-    sort(vec.begin(),vec.end());
-    cout << vec[m - n];
+    cout << the_kth_number_heap(vec, k);
 }
